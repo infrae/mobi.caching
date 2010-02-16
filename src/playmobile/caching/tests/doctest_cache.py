0174b2dcae5bdc6b3c9da909c27b2fd9cbf3d973
Antonin Amand <antonin.amand@gmail.com>
2010-02-16 14:58:13 +0100

    add doc test

---
+++ src/playmobile/caching/tests/doctest_cache.py
@@ -0,0 +1,48 @@
+"""
+    >>> from playmobile.caching import Cache
+    >>> from playmobile.caching.backend import NoCacheBackend, DictBackend
+
+    We instantiate a cache engine.
+
+    >>> cache_engine = Cache(namespace='somename')
+
+    By default cache backend is no cache.
+
+    >>> cache_engine.backend #doctest: +ELLIPSIS
+    <playmobile.caching.backend.NoCacheBackend object at ...>
+
+    >>> cache = cache_engine.cache
+
+    the cache method call the provided callable and cache it. But if the engine
+    is NoCacheBackend, nothing gets cached.
+
+    >>> cache('somekey', get_a)
+    a called
+    'a'
+    >>> cache('somekey', get_b)
+    b called
+    'b'
+
+    If we change the backend to a real cache backend. The callable is cached
+
+    >>> cache_engine.backend = DictBackend()
+    >>> cache('somekey', get_a)
+    a called
+    'a'
+    >>> cache('somekey', get_a)
+    'a'
+    >>> cache('somekey', get_b)
+    'a'
+    >>> cache('b key', get_b)
+    b called
+    'b'
+
+"""
+
+def get_a():
+    print "a called"
+    return "a"
+
+def get_b():
+    print "b called"
+    return "b"
